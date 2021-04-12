4d39fbd212f70db9c85407aa3fac28231f807572
Netanel Elikamel <netanelikamel@gmail.com>
2021-04-12 21:01:47 +0300

    nothing special

--- src/com/hit/games/TicTacTow.java
+++ src/com/hit/games/TicTacTow.java
@@ -40,20 +40,71 @@ public abstract class TicTacTow extends GameBoard {
 	
 	public GameState getGameState(GameMove move)
 	{
+		gameState=GameState.IN_PROGRESS;
 		if(updatePlayerMove(move))
 		{
-			
+			if(checkPlayerWin())
+				gameState=GameState.PLAYER_WON;
+			else if(checkPlayerLost())
+				gameState=GameState.PLAYER_LOST;
+			else if(checkTie())
+				gameState=GameState.TIE;
 		}
 		return gameState;
 	}
 	
+	public boolean checkPlayerWin()
+	{
+		if(		board[0][0]=='p' && board[0][1]=='p' && board[0][2]=='p' || 
+				board[1][0]=='p' && board[1][1]=='p' && board[1][2]=='p' ||
+				board[2][0]=='p' && board[2][1]=='p' && board[2][2]=='p' ||
+				board[0][0]=='p' && board[1][0]=='p' && board[2][0]=='p' ||
+				board[0][1]=='p' && board[1][1]=='p' && board[2][1]=='p' ||
+				board[0][2]=='p' && board[1][2]=='p' && board[2][2]=='p' ||
+				board[0][0]=='p' && board[1][1]=='p' && board[2][2]=='p' ||
+				board[0][2]=='p' && board[1][1]=='p' && board[2][0]=='p'     )
+			return true;
+		
+		return false;
+	}
+	
+	public boolean checkPlayerLost()
+	{
+		if(		board[0][0]=='c' && board[0][1]=='c' && board[0][2]=='c' || 
+				board[1][0]=='c' && board[1][1]=='c' && board[1][2]=='c' ||
+				board[2][0]=='c' && board[2][1]=='c' && board[2][2]=='c' ||
+				board[0][0]=='c' && board[1][0]=='c' && board[2][0]=='c' ||
+				board[0][1]=='c' && board[1][1]=='c' && board[2][1]=='c' ||
+				board[0][2]=='c' && board[1][2]=='c' && board[2][2]=='c' ||
+				board[0][0]=='c' && board[1][1]=='c' && board[2][2]=='c' ||
+				board[0][2]=='c' && board[1][1]=='c' && board[2][0]=='c'     )
+			return true;
+		
+		return false;
+	}
+	
+	public boolean checkTie()
+	{
+		if(board[0][0]!='b' && board[0][1]!='b' && board[0][2]!='b' && board[1][0]!='b' && board[1][1]!='b' && board[1][2]!='b' && board[2][0]!='b' && board[2][1]!='b' && board[2][2]!='b')
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	//updatePlayerMove Updates the player's move on the board. in case the move is not legal - nothing is done.
 	public boolean updatePlayerMove(GameMove move)
 	{
 		if(board[move.getRow()][move.getCol()]!='b')
+		{
+			gameState=GameState.ILLEGAL_PLAYER_MOVE;
 			return false;
-		else if()
-		
-		return false;
+		}
+		else
+		{
+			board[move.getRow()][move.getCol()]='b';
+			return true;
+		}
 	}
 	
 	public char[][] getBoardState()
@@ -74,127 +125,3 @@ public abstract class TicTacTow extends GameBoard {
 		System.out.println();
 	}
 }
-/*	
-	public IGameAlgo.GameState getGameState(GameBoard.GameMove move)
-	{
-		if(updatePlayerMove(move))
-		{
-			if(board[0][0]==board[0][1])
-			{
-				if(board[0][0]==board[0][2])
-				{
-					if(board[0][0]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[0][0]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}
-			
-			if(board[1][0]==board[1][1])
-			{
-				if(board[1][0]==board[1][2])
-				{
-					if(board[1][0]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[1][0]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}
-			
-			if(board[2][0]==board[2][1])
-			{
-				if(board[2][1]==board[2][2])
-				{
-					if(board[2][2]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[2][2]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}
-			
-			if(board[0][0]==board[1][0])
-			{
-				if(board[1][0]==board[2][0])
-				{
-					if(board[0][0]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[0][0]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}
-			
-			if(board[0][1]==board[1][1])
-			{
-				if(board[1][1]==board[2][1])
-				{
-					if(board[0][1]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[0][1]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}
-			
-			if(board[0][2]==board[1][2])
-			{
-				if(board[1][2]==board[2][2])
-				{
-					if(board[2][2]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[2][2]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}
-			
-			if(board[0][0]==board[1][1])
-			{
-				if(board[1][1]==board[2][2])
-				{
-					if(board[2][2]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[2][2]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}
-			
-			if(board[0][2]==board[1][1])
-			{
-				if(board[1][1]==board[2][0])
-				{
-					if(board[0][2]=='p')
-						game_state=GameState.PLAYER_WON;
-					if(board[0][2]=='c')
-						game_state=GameState.PLAYER_LOST;
-				}
-			}	
-		}	
-		else//if updatePlayerMove returns false
-			game_state=GameState.ILLEGAL_PLAYER_MOVE;
-		
-		//check if all cells is not BLANK
-		//if yes-game state is tie
-		int count=0;
-		for(int a=0;a<3;a++)
-			for(int b=0;b<3;b++)
-				if(board[a][b]!='b')
-					count++;
-		if(count==9)
-			game_state=GameState.TIE;
-		
-		return game_state;
-	}
-	
-	//"updatePlayerMove" Updates the player's move on the board
-	//in case that the move is illegal - nothing is done
-	//move - the player's move
-	//Returns true if the move is legal and false otherwise
-	public boolean updatePlayerMove(GameBoard.GameMove move)
-	{
-		if(move.getRow()>2||move.getRow()<0||move.getColumn()>2||move.getColumn()<0)
-			return false;
-		if(board[move.getRow()][move.getColumn()]!='b')
-			return false;
-		board[move.getRow()][move.getColumn()]='p';
-		return true;
-	}
-}
-*/
--- src/com/hit/games/TicTacTowRandom.java
+++ src/com/hit/games/TicTacTowRandom.java
@@ -7,6 +7,7 @@ public class TicTacTowRandom extends TicTacTow {
 		super(r, c);
 	}
 
+	//calcComputerMove Calculates the copmuter's move and updates the game board
 	@Override
 	public void calcComputerMove() 
 	{
